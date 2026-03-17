95c8279d8183b7428e82e19273149039fa09402e
Veeksha Pattikonda <veeksha@Veekshas-MacBook-Pro.local>
2026-03-16 23:03:46 -0500

    Final Submission - A5

--- src/test/java/playwrightLLM/PlaywrightLLMTest.java
+++ src/test/java/playwrightLLM/PlaywrightLLMTest.java
@@ -14,7 +14,7 @@ public class PlaywrightLLMTest {
         try (Playwright playwright = Playwright.create()) {
 
             Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions().setHeadless(false)
+                    new BrowserType.LaunchOptions().setHeadless(true)
             );
 
             BrowserContext context = browser.newContext(
